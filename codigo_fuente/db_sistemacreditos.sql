01ecdbd8ce45c86a1a5d85f6642e7b4965af8fe4
esCristian <cristianescuderoa@gmail.com>
2024-07-31 22:05:10 -0300

    Agregue base de datos

---
+++ codigo_fuente/db_sistemacreditos.sql
@@ -0,0 +1,213 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 01-08-2024 a las 03:01:41
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `db_sistemacreditos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `idCliente` bigint(20) NOT NULL,
+  `identificiacion` varchar(50) NOT NULL,
+  `nombre` varchar(200) NOT NULL,
+  `apellido` varchar(200) NOT NULL,
+  `telefono` bigint(20) DEFAULT NULL,
+  `email` varchar(200) NOT NULL,
+  `direccion` varchar(200) NOT NULL,
+  `nit` varchar(20) DEFAULT NULL,
+  `nombrefiscal` varchar(200) DEFAULT NULL,
+  `direccionfiscal` varchar(200) DEFAULT NULL,
+  `datecreate` datetime NOT NULL DEFAULT current_timestamp(),
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cuenta`
+--
+
+CREATE TABLE `cuenta` (
+  `idCuenta` bigint(20) NOT NULL,
+  `clienteid` bigint(20) NOT NULL,
+  `productoid` bigint(20) NOT NULL,
+  `frecuenciaid` bigint(20) NOT NULL,
+  `monto` decimal(10,0) NOT NULL,
+  `cuotas` int(11) NOT NULL,
+  `monto_cuotas` decimal(10,0) NOT NULL,
+  `cargo` decimal(10,0) NOT NULL,
+  `saldo` decimal(10,0) NOT NULL,
+  `datecreate` datetime NOT NULL DEFAULT current_timestamp(),
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `frecuencia`
+--
+
+CREATE TABLE `frecuencia` (
+  `idFrecencia` bigint(20) NOT NULL,
+  `frecuencia` varchar(200) NOT NULL,
+  `datecreate` datetime NOT NULL DEFAULT current_timestamp(),
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `movimiento`
+--
+
+CREATE TABLE `movimiento` (
+  `idMovimiento` int(11) NOT NULL,
+  `cuentaId` bigint(20) NOT NULL,
+  `tipomovimientoId` bigint(20) NOT NULL,
+  `monto` decimal(10,0) NOT NULL,
+  `descripvion` text NOT NULL,
+  `datecreate` datetime NOT NULL DEFAULT current_timestamp(),
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `idProducto` bigint(20) NOT NULL,
+  `codigo` varchar(200) NOT NULL,
+  `nombre` varchar(200) NOT NULL,
+  `descripcion` text NOT NULL,
+  `precio` decimal(10,0) NOT NULL,
+  `datecreate` datetime NOT NULL DEFAULT current_timestamp(),
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo:movimiento`
+--
+
+CREATE TABLE `tipo:movimiento` (
+  `idtipomovimiento` bigint(20) NOT NULL,
+  `movimiento` varchar(200) NOT NULL,
+  `tipo_movimiento` int(11) NOT NULL,
+  `descripcion` text NOT NULL,
+  `datecreate` datetime NOT NULL DEFAULT current_timestamp(),
+  `status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`idCliente`);
+
+--
+-- Indices de la tabla `cuenta`
+--
+ALTER TABLE `cuenta`
+  ADD PRIMARY KEY (`idCuenta`),
+  ADD KEY `clienteid` (`clienteid`),
+  ADD KEY `productoid` (`productoid`),
+  ADD KEY `frecuenciaid` (`frecuenciaid`);
+
+--
+-- Indices de la tabla `frecuencia`
+--
+ALTER TABLE `frecuencia`
+  ADD PRIMARY KEY (`idFrecencia`);
+
+--
+-- Indices de la tabla `movimiento`
+--
+ALTER TABLE `movimiento`
+  ADD PRIMARY KEY (`idMovimiento`),
+  ADD KEY `cuentaId` (`cuentaId`),
+  ADD KEY `tipomovimientoId` (`tipomovimientoId`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`idProducto`);
+
+--
+-- Indices de la tabla `tipo:movimiento`
+--
+ALTER TABLE `tipo:movimiento`
+  ADD PRIMARY KEY (`idtipomovimiento`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `idCliente` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cuenta`
+--
+ALTER TABLE `cuenta`
+  MODIFY `idCuenta` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `frecuencia`
+--
+ALTER TABLE `frecuencia`
+  MODIFY `idFrecencia` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `movimiento`
+--
+ALTER TABLE `movimiento`
+  MODIFY `idMovimiento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `idProducto` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo:movimiento`
+--
+ALTER TABLE `tipo:movimiento`
+  MODIFY `idtipomovimiento` bigint(20) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
